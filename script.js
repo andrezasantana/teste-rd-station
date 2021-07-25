2a445d9f1cef29ce248519622f3cc9075c07a795
Andreza Santana <andreza.santana16@gmail.com>
2021-07-25 18:35:32 -0300

    create section 2 form and adjust css

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function changeIcon() {
+// Function to change icon of menu hamburger
+function changeIconMenu() {
     document.getElementById("active").classList.toggle("icon-change");
 }
+// funtion to show password of form
+const btn = document.querySelector('#eye');
+btn.addEventListener('click', function () {
+    let passwordIntered = document.querySelector('#password');
+    let eyeIcon = document.querySelector('#eye');
+
+    if (passwordIntered.getAttribute('type') == 'password') {
+        passwordIntered.setAttribute('type', 'text');
+        eyeIcon.src = "/assets/open-eye.svg";
+    } else {
+        passwordIntered.setAttribute('type', 'password');
+        eyeIcon.src = "/assets/close-eye.svg";
+    }
+});
